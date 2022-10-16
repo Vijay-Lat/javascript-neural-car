e7febf0b0c3ab20722813c1ddac6cb2467dfc0b6
vijayamuralikrishna.jaisankar@impigertech.com <vijayamuralikrishna.jaisankar@impigertech.com>
2022-10-16 10:41:22 +0530

    add files

--- road.js
+++ road.js
@@ -6,22 +6,31 @@ class Road {
     this.left = x - width / 2;
     this.right = x + width / 2;
 
-    const infinity = 1/0.000000001
+    // const infinity = 1/0.000000001
+    const infinity = 10000000;
     this.top = -infinity;
     this.bottom = infinity;
+  }
+  getLaneCenter(laneIndex) {
+    const laneWidth = this.width / this.laneCount;
+    return this.left + laneWidth / 2 + Math.min(laneIndex,this.laneCount-1) * laneWidth;
   }
   draw(ctx) {
     console.log(this.left, this.top);
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
-    ctx.beginPath()
-    ctx.moveTo(this.left, this.top);
-    ctx.lineTo(this.left, this.bottom);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(this.right, this.top);
-    ctx.lineTo(this.right, this.bottom);
-    ctx.stroke();
+    for (let i = 0; i <= this.laneCount; i++) {
+      const x = lerp(this.left, this.right, i / this.laneCount);
+      if (i > 0 && i < this.laneCount) {
+        ctx.setLineDash([20, 20]);
+      } else {
+        ctx.setLineDash([]);
+      }
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
+    }
   }
 }
+
